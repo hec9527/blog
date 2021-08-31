5e00a7c4e1bb48e5d7a496cdefc3ae1418f0b685
hec9527 <hec9527@foxmail.com>
2021-08-31 14:36:42 +0800

    删除无用的文件

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ function HomepageHeader() {
         <h1 className='hero__title'>{siteConfig.title}</h1>
         <p className='hero__subtitle'>{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className='button button--secondary button--lg' to='/docs/intro'>
+          <Link className='button button--secondary button--lg' to='/blog'>
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
@@ -26,7 +26,7 @@ function HomepageHeader() {
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout title={`Hello from ${siteConfig.title}`} description='hec9527的博客'>
+    <Layout title={siteConfig.title}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
