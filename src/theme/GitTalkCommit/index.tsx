e64a924900c2d7cf45ff7e52142c53890475188d
hec9527 <hec9527@foxmail.com>
2021-12-10 11:04:40 +0800

    修改issues唯一的问题

--- src/theme/GitTalkCommit/index.tsx
+++ src/theme/GitTalkCommit/index.tsx
@@ -5,12 +5,12 @@ import 'gitalk/dist/gitalk.css';
 const GitTalkCommit: React.FC = () => {
   useEffect(() => {
     new Gitalk({
-      admin: ['hec9527'],
       clientID: '776945e61c309bf840cb',
       clientSecret: '1488e71a045da725e1d167bb57775ec744c66936',
       repo: 'blog',
       owner: 'hec9527',
-      id: 'hec9527-blog',
+      admin: ['hec9527'],
+      id: location.pathname,
       distractionFreeMode: true,
     }).render('gitalk-container');
   }, []);
