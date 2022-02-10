14933b57be83b28ca079dd9f71a8293fcb75834d
hec9527 <hec9527@foxmail.com>
2022-02-10 18:59:18 +0800

    修复broken链接

--- src/pages/test.js
+++ src/pages/test.js
@@ -13,7 +13,7 @@ function HomepageHeader() {
         <h1 className='hero__title'>{siteConfig.title}</h1>
         <p className='hero__subtitle'>{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className='button button--secondary button--lg' to='/blog'>
+          <Link className='button button--secondary button--lg' to='/'>
             前往博客
           </Link>
         </div>
