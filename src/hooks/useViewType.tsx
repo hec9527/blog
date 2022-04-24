6c97dbbb9d3d1fb952f2aaa4243796414a3469b4
hec9527 <hec9527@foxmail.com>
2022-04-24 19:55:01 +0800

    Revert "修改列表展示形式逻辑,列表模式不会闪烁"
    This reverts commit bd31c8b488ba0706b844a503bba4c14b6eb123f3.

--- src/hooks/useViewType.tsx
+++ src/hooks/useViewType.tsx
@@ -1,12 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function useViewType() {
-  const [type, setType] = useState<ViewType>((localStorage.getItem('VIEW_TYPE') as ViewType) || 'card');
+  const [type, setType] = useState<ViewType>('card');
 
   const toggleType = (type: ViewType) => {
     localStorage.setItem('VIEW_TYPE', type);
     setType(type);
   };
+
+  useEffect(() => {
+    setType((localStorage.getItem('VIEW_TYPE') as ViewType) || 'card');
+  }, []);
 
   return [type, toggleType] as const;
 }
